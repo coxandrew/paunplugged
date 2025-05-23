133cd1cf4f55afe737fccb822f86b340bbc17397
Andrew Cox <andrew@andrewcox.org>
2025-05-23 09:53:27 -0400

    Update script to pull from latest spreadsheet

--- update_families.sh
+++ update_families.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set variables
-SHEET_URL="https://docs.google.com/spreadsheets/d/1-8dfLXeE5hP2-KMeickluKh7Pf7gHPi-Eg2idPa3l8s/pub?output=csv"
-DOWNLOAD_PATH="/Users/cox/projects/sewickleyunplugged/_data/families.csv"
-REPO_PATH="/Users/cox/projects/sewickleyunplugged"
+SHEET_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vRobn7BUacujm5NdYbY4E5YthDB3oCjXqv5H81AzA2h-l-ez8mJ3oy_77acmqPdvc7_VeZ9bgCPKLqp/pub?gid=2124828097&single=true&output=csv"
+DOWNLOAD_PATH="/Users/cox/projects/paunplugged/_data/supporters.csv"
+REPO_PATH="/Users/cox/projects/paunplugged"
 
 # Download the Google Sheet as a CSV
 curl -L -o "$DOWNLOAD_PATH" "$SHEET_URL"
@@ -15,7 +15,13 @@ cd "$REPO_PATH" || exit
 git add "$DOWNLOAD_PATH"
 
 # Create a new commit
-git commit -m "Update families.csv with the latest data"
+git commit -m "Update supporters.csv with the latest data"
+
+# Check if there are any unpushed local commits
+if git status | grep -q "Your branch is ahead of"; then
+    echo "Warning: There are unpushed local commits. Skipping push to master."
+    exit 1
+fi
 
 # Push the commit to the master branch
 git push origin master
